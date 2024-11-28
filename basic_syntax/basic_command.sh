70fa43433935890456d5f64823acf480016d8df9
HanSungMin99 <sungmini9876@gmail.com>
2024-11-28 14:39:18 +0900

    변경후

--- basic_syntax/basic_command.sh
+++ basic_syntax/basic_command.sh
@@ -24,6 +24,11 @@ git log --graph
 
 #원격저장소로 업로드
 git push oringin 브랜치명
-#충돌발생시 무시하고 강제push
+#충돌발생시 무시하고 강제push, 로컬커밋 기준으로 원격 덮어쓰기
 git push origin main --force
+
+#pull은 원격의 커밋사항을 local로 다운로드(working directory까지 반영)
+git pull origin main
+
+#특정 commit ID로의 
 
